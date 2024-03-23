9d4763ac124f892b4e6f06fd9cf77307d3d25121
Georgy Yuriev <georgy.yuriev@gmail.com>
2024-03-23 20:55:53 +0500

    stream flash as part of turbo_stream answer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :error, :success
+  helper_method :show_flash
+
+  def show_flash(...)
+    turbo_stream.append(:flash_container) do
+      render Flash::Component.new(...)
+    end
+  end
 end
--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,8 @@
 class DemoController < ApplicationController
   def index
     flash.now[:success] = "Welcome to the Demo!", 'Notification title'
+    @flash_types = Types::FlashType.values
   end
+
+  def streamer; end
 end
